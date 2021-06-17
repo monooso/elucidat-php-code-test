fa9cb7de03876df5d6168106a726752b4bf822be
Stephen Lewis <code@stephenlewis.me>
2021-06-17 19:43:48 +0100

    Fix "broken" test
    This is tricky. There appears to be a flaw in the businesses logic for certain
    item types. Some quality updates are calculated before decrementing the lifespan,
    others are calculated after.
    
    In the absence of a domain expert to consult, I’ve decided to regard this as a
    bug in the current implementation.

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -4,6 +4,7 @@ namespace Specs;
 
 use App\GildedRose;
 use App\Item;
+use App\ItemTypes\EventItem;
 use App\ItemTypes\LegendaryItem;
 use App\ItemTypes\SimpleItem;
 
@@ -101,55 +102,55 @@ describe('Gilded Rose', function () {
         });
         context('Backstage Passes', function () {
             it('updates Backstage pass items long before the sell date', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 11)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 10, 12)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(11);
-                expect($gr->getItem(0)->sellIn)->toBe(10);
+                expect($gr->getItem(0)->sellIn)->toBe(11);
             });
             it('updates Backstage pass items close to the sell date', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 10, 10)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(12);
                 expect($gr->getItem(0)->sellIn)->toBe(9);
             });
             it('updates Backstage pass items close to the sell data, at max quality', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 50, 10)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 50, 10)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(50);
                 expect($gr->getItem(0)->sellIn)->toBe(9);
             });
             it('updates Backstage pass items very close to the sell date', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 5)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 10, 5)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(13); // goes up by 3
                 expect($gr->getItem(0)->sellIn)->toBe(4);
             });
             it('updates Backstage pass items very close to the sell date, at max quality', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 50, 5)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 50, 5)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(50);
                 expect($gr->getItem(0)->sellIn)->toBe(4);
             });
             it('updates Backstage pass items with one day left to sell', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 1)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 10, 1)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(13);
                 expect($gr->getItem(0)->sellIn)->toBe(0);
             });
             it('updates Backstage pass items with one day left to sell, at max quality', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 50, 1)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 50, 1)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(50);
                 expect($gr->getItem(0)->sellIn)->toBe(0);
             });
             it('updates Backstage pass items on the sell date', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 10, 0)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(0);
                 expect($gr->getItem(0)->sellIn)->toBe(-1);
             });
             it('updates Backstage pass items after the sell date', function () {
-                $gr = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, -1)]);
+                $gr = new GildedRose([new EventItem('Backstage passes to a TAFKAL80ETC concert', 10, -1)]);
                 $gr->nextDay();
                 expect($gr->getItem(0)->quality)->toBe(0);
                 expect($gr->getItem(0)->sellIn)->toBe(-2);
