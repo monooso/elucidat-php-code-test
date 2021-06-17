e9913e05f1b6d7b45845fa64127ed09bc12e561f
Stephen Lewis <code@stephenlewis.me>
2021-06-17 20:00:11 +0100

    Remove remaining logic from GildedRose

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\ItemTypes\ItemInterface;
-
 class GildedRose
 {
     public function __construct(private array $items)
@@ -22,34 +20,7 @@ class GildedRose
     public function nextDay()
     {
         foreach ($this->items as $item) {
-            if ($item instanceof ItemInterface) {
-                $item->nextDay();
-                continue;
-            }
-
-            if ($item->name != 'Aged Brie') {
-                if ($item->quality > 0) {
-                    $item->quality = $item->quality - 1;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
-            }
-
-            $item->sellIn = $item->sellIn - 1;
-
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+            $item->nextDay();
         }
     }
 }
