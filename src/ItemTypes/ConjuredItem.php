4cd2d12555b0983b61f4e14c1a3e378507cac9fd
Stephen Lewis <code@stephenlewis.me>
2021-06-22 12:00:12 +0100

    Remove duplicate traits

--- src/ItemTypes/ConjuredItem.php
+++ src/ItemTypes/ConjuredItem.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace App\ItemTypes;
 
 use App\Contracts\ItemInterface;
-use App\Traits\HasDecreasingLifespan;
-use App\Traits\NormalizesQuality;
 
 final class ConjuredItem extends BaseItem implements ItemInterface
 {
-    use HasDecreasingLifespan;
-    use NormalizesQuality;
-
     protected function calculateQuality(int $quality, int $lifespan): int
     {
         $quality -= ($lifespan < 0 ? 4 : 2);
--- src/ItemTypes/SimpleItem.php
+++ src/ItemTypes/SimpleItem.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace App\ItemTypes;
 
 use App\Contracts\ItemInterface;
-use App\Traits\HasDecreasingLifespan;
 
 final class SimpleItem extends BaseItem implements ItemInterface
 {
-    use HasDecreasingLifespan;
-
     protected function calculateQuality(int $quality, int $lifespan): int
     {
         $quality -= ($lifespan < 0 ? 2 : 1);
